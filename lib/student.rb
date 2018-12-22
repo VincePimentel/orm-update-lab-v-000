00be378e469ee778fff079f906df3389bbec4dd2
Vince Andrew Pimentel <contact@vincep.me>
2018-12-22 02:10:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,10 +56,12 @@ class Student
   def self.create(name, grade)
     student = self.new(name, grade)
     student.save
+    student
   end
 
   def self.new_from_db(row)
     student = self.new(row[1], row[2], row[0])
+    student
   end
 
   def self.find_by_name(name)
